46f10534a335dfee46c6e8c0984ec274ea21b33f
cuprumtan <cuprumtan@gmail.com>
2020-11-28 01:11:32 +0300

    Добавленло самостоятельное определение количества features

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 import sklearn.ensemble as ske
 from sklearn import tree, linear_model, svm
-from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import SelectFromModel, SelectKBest
 from sklearn.model_selection import train_test_split
 import joblib
 from sklearn.naive_bayes import GaussianNB
@@ -11,33 +11,55 @@ from sklearn.naive_bayes import GaussianNB
 
 # Read data.csv file
 data = pd.read_csv('dataset.csv', sep=',')
-X = data.drop(['ID', 'target'], axis=1).values
-y = data['target'].values
+# X = data.drop(['ID', 'target'], axis=1).values
+# y = data['target'].values
 
+# ==================================================================
+number_of_features = 10
+X = data.iloc[:, 1:15]  # drop 'ID' and 'target'
+y = data.iloc[:, -1]    # features columns
+fsel = SelectKBest(k=number_of_features).fit(X, y)
+X_new = fsel.transform(X)
+dfscores = pd.DataFrame(fsel.scores_)
+dfcolumns = pd.DataFrame(X.columns)
+featureScores = pd.concat([dfcolumns, dfscores], axis=1)
 print('Searching important feature based on %i total features\n' % X.shape[1])
+print('10 features identified as important:')
+featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+print(featureScores.nlargest(number_of_features, 'Score'))  # print 10 best features
+features = [x[0]for x in featureScores.nlargest(number_of_features, 'Score')[['Specs']].to_numpy()]
 
-# Feature selection using Trees Classifier
-fsel = ske.ExtraTreesClassifier().fit(X, y)
-model = SelectFromModel(fsel, prefit=True)
-X_new = model.transform(X)
-nb_features = X_new.shape[1]
 
 # Train test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X_new, y, test_size=0.2)
+    X_new, y, random_state=0)
 
-features = []
+# ==================================================================
 
-print('%i features identified as important:' % nb_features)
-
-indices = np.argsort(fsel.feature_importances_)[::-1][:nb_features]
-for f in range(nb_features):
-    print("%d. feature %s (%f)" % (
-        f + 1, data.columns[2 + indices[f]], fsel.feature_importances_[indices[f]]))
-
-# Take care of the feature order
-for f in sorted(np.argsort(fsel.feature_importances_)[::-1][:nb_features]):
-    features.append(data.columns[2 + f])
+# print('Searching important feature based on %i total features\n' % X.shape[1])
+# # Feature selection using Trees Classifier
+# fsel = ske.ExtraTreesClassifier().fit(X, y)
+# model = SelectFromModel(fsel, prefit=True)
+# X_new = model.transform(X)
+# nb_features = X_new.shape[1]
+#
+#
+# # Train test split
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X_new, y, random_state=0)
+#
+# features = []
+#
+# print('%i features identified as important:' % nb_features)
+#
+# indices = np.argsort(fsel.feature_importances_)[::-1][:nb_features]
+# for f in range(nb_features):
+#     print("%d. feature %s (%f)" % (
+#         f + 1, data.columns[2 + indices[f]], fsel.feature_importances_[indices[f]]))
+#
+# # Take care of the feature order
+# for f in sorted(np.argsort(fsel.feature_importances_)[::-1][:nb_features]):
+#     features.append(data.columns[2 + f])
 
 # Compare algorithms
 algorithms = {
